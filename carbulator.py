823f922b46d321ed19fe3b57dd412d4e93f8e346
dmac3000 <13706@coderacademy.edu.au>
2022-12-10 10:51:33 +1100

    created function for calculating carbs

--- carbulator.py
+++ carbulator.py
@@ -8,3 +8,51 @@ def print_options():
     opt = input("Select your option (1-3): ")
     return opt
 option = ""
+
+# Define a function to calculate carb intake
+def calc_carb_intake(weight, height, age, gender, activity_level):
+  global der
+  # Check if the input values are valid
+  if weight <= 0 or height <= 0 or age <= 0:
+    # If any of the input values are not valid, raise an error
+    raise ValueError("Invalid input values. Weight, height, and age must be positive numbers.")
+
+#   # Calculate the body mass index (BMI) using the formula:
+#   # BMI = weight (kg) / height^2 (m^2)
+#   bmi = weight / math.pow(height, 2)
+
+  # Calculate the basal metabolic rate (BMR) using the formula:
+  # BMR = 10 * weight (kg) + 6.25 * height (cm) - 5 * age (years) + s
+  # where s is the value of 5 for males and -161 for females
+  if gender == "male":
+    s = 5
+  elif gender == "female":
+    s = -161
+  else:
+    # If the gender is not male or female, raise an error
+    raise ValueError("Gender must be either male or female for the purposes of this app.")
+
+#  Calculate BMR using the Mifflin-St Jeor Equation:
+  bmr = 10 * weight + 6.25 * height - 5 * age + s
+  
+  # Calculate the daily energy requirement (DER) using the formula:
+  # DER = BMR * activity_level
+  # where activity_level is the value of 1.2 for sedentary, 1.375 for lightly active, 1.55 for moderately active, and 1.725 for very active
+
+  if activity_level == "1":
+    der = bmr * 1.2
+  elif activity_level == "2":
+    der = bmr * 1.375
+  elif activity_level == "3":
+    der = bmr * 1.55
+  elif activity_level == "4":
+    der = bmr * 1.725
+  else:
+    # If the activity level is not valid, raise an error
+    raise ValueError("Invalid activity level. Activity level must be sedentary, lightly active, moderately active, or very active.")
+
+  # Calculate the daily carb intake using the formula:
+  # Daily carb intake = der * 0.4 / 4 (kcal/day)
+  daily_carb_intake = der * 0.4 / 4
+  # Return the calculated value
+  return daily_carb_intake
--- test_print_options.py
+++ test_print_options.py
@@ -1,8 +1,10 @@
 from carbulator import print_options
+from io import StringIO
+import sys
 
-def test_print_options():
-    # Call the function and save the selected option
-    option = print_options()
-
-    # Check if the selected option is returned correctly
-    assert option == 1
+def test_print_options(capsys):
+    # Test that the function prints the expected menu options
+    print_options()
+    captured_output = capsys.readouterr()
+    assert captured_output.out == "Welcome to the Carbulator! Select a menu option to continue...\n1. Calculate my recommended daily carb intake\n2. Track my weekly achieved carb intake\n3. See my achieved intake vs. my goal\n4. Exit\n"
+    
